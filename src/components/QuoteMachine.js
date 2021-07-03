06b505785a58a16ca15935483ebc4763767d845a
Nahuel <>
2021-07-03 03:33:24 -0300

    ready to deploy

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -3,6 +3,9 @@ import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions  from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
+import IconButton from "@material-ui/core/IconButton";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTwitter} from '@fortawesome/free-brands-svg-icons'
 
 
 class QuoteMachine extends React.Component {
@@ -22,13 +25,12 @@ class QuoteMachine extends React.Component {
                     <span id='text'>{quote}</span>
                     <br/>
                     <span id='author'>{author}</span>
-                    <br/>
                 </CardContent>
                 <CardActions>
                     <Button onClick={this.props.clickHandler} id="new-quote">Next Quote</Button>
-                    <br/>
-                    <a id='tweet-quote' href={href_tweet}>tw</a>
-                    <br/> 
+                    <IconButton id='tweet-quote' href={encodeURI(href_tweet)}>
+                        <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+                    </IconButton>
                 </CardActions>
             </Card>    
         );
